d314cbbb49c4a8f0942195a1d5fa85efd8f30acd
Manasses_M_Lira <manassesmlira@gmail.com>
2023-04-17 12:32:43 -0300

    iniciando as configuações do carrinho

--- script.js
+++ script.js
@@ -208,3 +208,17 @@ function updateCart() {
         c('aside').classList.remove('show')
     }
 }
+
+function updateCart() {
+    if(cart.length > 0) {
+        c('aside').classList.add('show')
+        for(let i in cart) {
+            let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id)
+
+            console.log(pizzaItem)
+        }
+    } else {
+        c('aside').classList.remove('show')
+    }
+}
+
