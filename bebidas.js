4d3306c63064b2fe617b0181c67b5f3229f4eb06
Manasses_M_Lira <manassesmlira@gmail.com>
2023-04-23 01:26:27 -0300

    nome super pizza

--- bebidas.js
+++ bebidas.js
@@ -1,13 +1,13 @@
 let bebidaJson = [
-    {id:1, name:'Coca-Cola', img:'images/cocacola.png', price:20.19, sizes:['2L'], description:'Descrição da bebida em mais de uma linha muito legal bem interessante'},
+    {id:1, name:'Coca-Cola', img:'images/cocacola.png', price:14.00, sizes:['2L'], description:'Descrição da bebida em mais de uma linha muito legal bem interessante'},
    
-    {id:2, name:'Pepsi', img:'images/pepsi.png', price:18.00, sizes:['2L'], description:'Descrição da bebida em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Pepsi', img:'images/pepsi.png', price:12.00, sizes:['2L'], description:'Descrição da bebida em mais de uma linha muito legal bem interessante'},
   
-    {id:3, name:'Fanta Uva', img:'images/uva.png', price:17.45, sizes:['2L'], description:'Descrição da bebida em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Fanta Uva', img:'images/uva.png', price:10.00, sizes:['2L'], description:'Descrição da bebida em mais de uma linha muito legal bem interessante'},
  
-    {id:4, name:'Fanta Laranja', img:'images/laranja.png', price:19.77, sizes:['2L'], description:'Descrição da bebida em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Fanta Laranja', img:'images/laranja.png', price:10.00, sizes:['2L'], description:'Descrição da bebida em mais de uma linha muito legal bem interessante'},
  
-    {id:5, name:'Guaraná Antártica', img:'images/guarana.png', price:21.43, sizes:['2L'], description:'Descrição da bebida em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Guaraná Antártica', img:'images/guarana.png', price:12.00, sizes:['2L'], description:'Descrição da bebida em mais de uma linha muito legal bem interessante'},
  
     
 ];
